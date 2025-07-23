0e75dcb5562b40e33e91f4c60abc807363510c6f
wangxinbbb <1399388692@qq.com>
2025-07-23 16:09:10 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["222.173.93.251"];
+pub const RS_PUB_KEY: &str = "TDaUS0rUUc3aMgeAd6nNNR5H9k0AC3IQA3w7A61n59k=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
